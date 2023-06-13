fbd81007b6e073320d5e6ee5e45397e8d9812c56
David Smith <96678397+Moomins07@users.noreply.github.com>
2023-06-13 12:50:28 +0100

    usual update

--- 12-oop-classes-private-properties/03-static-methods/script.js
+++ 12-oop-classes-private-properties/03-static-methods/script.js
@@ -4,4 +4,17 @@ class Rectangle {
     this.height = height;
     this.width = width;
   }
+
+  area() {
+    return this.height * this.width;
+  }
+
+  // Static method -  Not using data from constructor function, just simply returns our class name
+  static getClass() {
+    return 'Rectangle';
+  }
 }
+
+const rect = new Rectangle('Rect', 10, 10);
+console.log(rect.area());
+console.log(Rectangle.getClass());
